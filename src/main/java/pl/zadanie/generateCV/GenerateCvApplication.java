6e14b656e44b01cfb95545e2cd8c66b87b0ade73
rejbo.dawid@gmail.com <rejbo.dawid@gmail.com>
2018-09-30 12:38:29 +0200

    first commit

---
+++ src/main/java/pl/zadanie/generateCV/GenerateCvApplication.java
@@ -0,0 +1,12 @@
+package pl.zadanie.generateCV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateCvApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/zadanie/generateCV/GenerateCvApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.zadanie.generateCV;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GenerateCvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
